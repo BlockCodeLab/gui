3a76a371c4b83710399df63fede05f0bfb75f374
Yeqin Gong <gon@gyeq.in>
2024-01-26 17:12:47 +0800

    add context menu for library item

--- packages/ui/src/library/library.jsx
+++ packages/ui/src/library/library.jsx
@@ -1,6 +1,6 @@
 import classNames from 'classnames';
 import { useState, useEffect } from 'preact/hooks';
-import { Button } from '../button/button';
+import { ContextMenu } from '../context-menu/context-menu';
 import { Modal } from '../modal/modal';
 import { Spinner } from '../spinner/spinner';
 import LibraryItem from './library-item';
@@ -59,27 +59,31 @@ export function Library({ title, filterable, filterPlaceholder, emptyText, tags,
       >
         {data && data.length > 0 ? (
           data.map((dataItem, index) => (
-            <LibraryItem
-              id={index}
+            <ContextMenu
+              menuItems={dataItem.contextMenu}
               key={index}
-              hidden={dataItem.hidden}
-              disabled={dataItem.disabled}
-              preview={dataItem.preview}
-              featured={dataItem.featured}
-              icon={dataItem.icon}
-              image={dataItem.image}
-              name={dataItem.name}
-              description={dataItem.description}
-              extensionId={dataItem.extensionId}
-              internetRequired={dataItem.internetRequired}
-              usbRequired={dataItem.usbRequired}
-              bluetoothRequired={dataItem.bluetoothRequired}
-              circuitRequired={dataItem.circuitRequired}
-              collaborator={dataItem.collaborator}
-              onMouseEnter={dataItem.onMouseEnter}
-              onMouseLeave={dataItem.onMouseLeave}
-              onSelect={dataItem.onSelect}
-            />
+            >
+              <LibraryItem
+                id={index}
+                hidden={dataItem.hidden}
+                disabled={dataItem.disabled}
+                preview={dataItem.preview}
+                featured={dataItem.featured}
+                icon={dataItem.icon}
+                image={dataItem.image}
+                name={dataItem.name}
+                description={dataItem.description}
+                extensionId={dataItem.extensionId}
+                internetRequired={dataItem.internetRequired}
+                usbRequired={dataItem.usbRequired}
+                bluetoothRequired={dataItem.bluetoothRequired}
+                circuitRequired={dataItem.circuitRequired}
+                collaborator={dataItem.collaborator}
+                onMouseEnter={dataItem.onMouseEnter}
+                onMouseLeave={dataItem.onMouseLeave}
+                onSelect={dataItem.onSelect}
+              />
+            </ContextMenu>
           ))
         ) : (
           <div className={styles.spinnerWrapper}>
