50f7b90dd2d78a816a3523da5421203646865f25
Yeqin Gong <gon@gyeq.in>
2024-04-01 22:53:55 +0800

    update runtime

--- packages/workspace-tankwar/src/components/tankwar-player/ai.js
+++ packages/workspace-tankwar/src/components/tankwar-player/ai.js
@@ -2,13 +2,13 @@ export const simple = (color, id) => `
 ((tank) => {
   runtime.openEventsGroup('${color}_${id}');
 
-  runtime.on('start', async function anonymous() {
+  runtime.when('start', async function anonymous() {
     await tank.util.turnRight(45);
     tank.util.speed = 100;
     await runtime.sleep(2.5);
     tank.util.speed = 0;
     await tank.util.turnLeft(90);
-    runtime.broadcast('@ai_simple_${color}_${id}_message_1');
+    runtime.fire('@ai_simple_${color}_${id}_message_1');
     while (true) {
       tank.util.speed = 100;
       await runtime.sleep(5.5);
@@ -18,8 +18,8 @@ export const simple = (color, id) => `
       await runtime.nextFrame();
     }
   });
-  
-  runtime.on('@ai_simple_${color}_${id}_message_1', async function anonymous(done) {
+
+  runtime.when('@ai_simple_${color}_${id}_message_1', async function anonymous(done) {
     while (true) {
       if ((await tank.util.scan(0) !== Infinity)) {
         await tank.util.attack(0, (await tank.util.scan(0)));
@@ -50,7 +50,7 @@ export const simple = (color, id) => `
     }
     done()
   });
-  
+
   runtime.closeEventsGroup();
 })(runtime.${color}Tank);
 `;
@@ -61,13 +61,13 @@ export const medium = (color, id) => `
 
   let _health_ = 0;
   let _angle_ = 0;
-  
-  runtime.on('start', async function anonymous() {
+
+  runtime.when('start', async function anonymous() {
     _angle_ = runtime.random(1, 360);
-    runtime.broadcast('@ai_medium_${color}_${id}_start')
+    runtime.fire('@ai_medium_${color}_${id}_start')
   });
-  
-  runtime.on('start', async function anonymous() {
+
+  runtime.when('start', async function anonymous() {
     while (true) {
       _health_ = tank.util.health;
       while (!((tank.util.health < _health_))) {
@@ -78,13 +78,13 @@ export const medium = (color, id) => `
       tank.util.move(runtime.random(1, 360), 100);
       await runtime.sleep(1);
       tank.util.speed = 0;
-      runtime.broadcast('@ai_medium_${color}_${id}_start')
+      runtime.fire('@ai_medium_${color}_${id}_start')
       if (anonymous.aborted) return;
       await runtime.nextFrame();
     }
   });
-  
-  runtime.on('@ai_medium_${color}_${id}_start', async function anonymous(done) {
+
+  runtime.when('@ai_medium_${color}_${id}_start', async function anonymous(done) {
     while (true) {
       while (!((await tank.util.scan(_angle_) !== Infinity))) {
         _angle_ = (isNaN(_angle_) ? 0 : +_angle_) + +(10);
@@ -106,7 +106,7 @@ export const medium = (color, id) => `
     }
     done()
   });
-  
+
   runtime.closeEventsGroup();
 })(runtime.${color}Tank);
 `;
@@ -117,9 +117,9 @@ export const senior = (color, id) => `
 
   let _angle_ = 0;
   let _health_ = 0;
-  
-  
-  runtime.on('start', async function anonymous() {
+
+
+  runtime.when('start', async function anonymous() {
     if (((tank.util.y < '5') && (tank.util.y > '-5'))) {
       await tank.util.turnLeft(104);
     }
@@ -132,10 +132,10 @@ export const senior = (color, id) => `
       await runtime.nextFrame();
     }
     tank.util.speed = 0;
-    runtime.broadcast('@ai_senior_${color}_${id}_start')
+    runtime.fire('@ai_senior_${color}_${id}_start')
   });
-  
-  runtime.on('@ai_senior_${color}_${id}_start', async function anonymous(done) {
+
+  runtime.when('@ai_senior_${color}_${id}_start', async function anonymous(done) {
     if ((tank.util.x < '0')) {
       if ((tank.util.y < '0')) {
         _angle_ = '45';
@@ -153,7 +153,7 @@ export const senior = (color, id) => `
       }
     }
     await tank.util.setDirection(_angle_);
-    runtime.broadcast('@ai_senior_${color}_${id}_watch_health');
+    runtime.fire('@ai_senior_${color}_${id}_watch_health');
     _angle_ = (isNaN(_angle_) ? 0 : +_angle_) + +(-60);
     for (let _ = 0; _ < 78; _++) {
       if (((await tank.util.scan(_angle_)) < '400') && ((await tank.util.scan(_angle_)) > '100')) {
@@ -164,11 +164,11 @@ export const senior = (color, id) => `
       if (anonymous.aborted) return;
       await runtime.nextFrame();
     }
-    runtime.broadcast('@ai_senior_${color}_${id}_next_place');
+    runtime.fire('@ai_senior_${color}_${id}_next_place');
     done();
   });
-  
-  runtime.on('@ai_senior_${color}_${id}_next_place', async function anonymous(done) {
+
+  runtime.when('@ai_senior_${color}_${id}_next_place', async function anonymous(done) {
     runtime.abort(anonymous.id);
     if ((runtime.random(1, 3) == '1')) {
       await tank.util.turnLeft(45);
@@ -207,20 +207,20 @@ export const senior = (color, id) => `
       }
     }
     tank.util.speed = 0;
-    runtime.broadcast('@ai_senior_${color}_${id}_start')
+    runtime.fire('@ai_senior_${color}_${id}_start')
     done()
   });
-  
-  runtime.on('@ai_senior_${color}_${id}_watch_health', async function anonymous(done) {
+
+  runtime.when('@ai_senior_${color}_${id}_watch_health', async function anonymous(done) {
     _health_ = tank.util.health;
     while (!((tank.util.health < _health_))) {
       if (anonymous.aborted) return;
       await runtime.nextFrame();
     }
-    runtime.broadcast('@ai_senior_${color}_${id}_next_place')
+    runtime.fire('@ai_senior_${color}_${id}_next_place')
     done()
   });
-  
+
   runtime.closeEventsGroup();
 })(runtime.${color}Tank);
 `;
