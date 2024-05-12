d1c8c44752600e18f624efb551daa84c20554936
Yeqin Gong <gon@gyeq.in>
2024-05-13 01:15:07 +0800

    fixed empty string

--- packages/blocks-player/src/generators/javascript/text.js
+++ packages/blocks-player/src/generators/javascript/text.js
@@ -2,6 +2,6 @@ import { javascriptGenerator } from './generator';
 
 javascriptGenerator['text'] = (block) => {
   const textValue = block.getFieldValue('TEXT');
-  const code = isNaN(textValue) ? javascriptGenerator.quote_(textValue) : +textValue;
+  const code = textValue.length === 0 || isNaN(textValue) ? javascriptGenerator.quote_(textValue) : +textValue;
   return [code, javascriptGenerator.ORDER_ATOMIC];
 };
--- packages/workspace-blocks/src/generators/python/text.js
+++ packages/workspace-blocks/src/generators/python/text.js
@@ -2,6 +2,6 @@ import { pythonGenerator } from './generator';
 
 pythonGenerator['text'] = (block) => {
   const textValue = block.getFieldValue('TEXT');
-  const code = isNaN(textValue) ? pythonGenerator.quote_(textValue) : +textValue;
+  const code = textValue.length === 0 || isNaN(textValue) ? pythonGenerator.quote_(textValue) : +textValue;
   return [code, pythonGenerator.ORDER_ATOMIC];
 };
