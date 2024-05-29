b60720b98943594cfcc446a60ea53f335a37176a
Yeqin Gong <gon@gyeq.in>
2024-05-29 15:04:36 +0800

    add text extension

---
+++ extensions/text/README.md
@@ -0,0 +1,13 @@
+# @blockcode/extension-text
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To development:
+
+```bash
+bun run dev
+```
---
+++ extensions/text/src/index.js
@@ -0,0 +1,23 @@
+import featureImage from './feature.svg';
+import iconImage from './icon.svg';
+
+export default {
+  disabled: true,
+  name: 'Animated Text',
+  description: 'Bring words to life.',
+  image: featureImage,
+  icon: iconImage,
+  tags: ['blocks', 'arcade', 'display'],
+
+  // l10n
+  translations: {
+    en: {
+      name: 'Animated Text',
+      description: 'Bring words to life.',
+    },
+    'zh-Hans': {
+      name: '动画文字',
+      description: '让文字鲜活起来。',
+    },
+  },
+};
