070e913d1dc848806bb9c708aa3e93d606d07497
Yeqin Gong <gon@gyeq.in>
2025-01-07 23:15:23 +0800

    添加消息模版

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,10 @@
 import './l10n';
 
 import { render } from 'preact';
-import { addAlertConfig, LocalesProvider, AppProvider, ProjectProvider, Text, Spinner } from '@blockcode/core';
+import { LocalesProvider, AppProvider, ProjectProvider } from '@blockcode/core';
 import { Layout } from './components/layout/layout';
 
-// 添加“导入”消息模版
-addAlertConfig('importing', {
-  icon: <Spinner level="success" />,
-  message: (
-    <Text
-      id="gui.alert.importing"
-      defaultMessage="importing..."
-    />
-  ),
-});
+import './lib/alert-messages';
 
 function App() {
   return (
